60f21a2ba236cf7aa61042870faa052c63d69880
bunlong22 <suonbunlong93@gmail.com>
2023-07-18 23:03:07 +0700

    the index method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,6 +13,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::all();
+        return view('category.index')->with('categories', $categories);
     }
 
     /**
