40d23962ca19aac18a925249965975ae62cdcd94
bunlong22 <suonbunlong93@gmail.com>
2023-07-18 22:14:29 +0700

    add the model of the table test

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Test extends Model
 {
     use HasFactory;
+    protected $table = "test"; // the test table
+    protected $fillable = [ //the table coloumn
+        'name',
+        'description',
+        'done',
+    ];
 }
