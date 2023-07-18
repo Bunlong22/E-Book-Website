bc1b32e231d26c66a01d08210ecb301ab3fa0c25
bunlong22 <suonbunlong93@gmail.com>
2023-07-18 22:27:45 +0700

    using the test model & print all the record

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Test;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/testmodel', function () {
+    $testing = Test::findOrFail(1);
+    echo ($testing->name);
+    echo "<br/>" . ($testing->description);
+    echo "<br/>" . ($testing->done);
 });
